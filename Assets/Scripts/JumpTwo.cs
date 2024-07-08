8a9937d4f5281a1ad8792031042c19f131c30d30
AngelaMaria-Arba <141113992+AngelaMaria-Arba@users.noreply.github.com>
2024-07-08 18:22:31 +0530

    add

--- Assets/Scripts/JumpTwo.cs
+++ Assets/Scripts/JumpTwo.cs
@@ -7,96 +7,65 @@ using Unity.MLAgents.Sensors;
 
 public class JumpTwo : Agent
 {
-    // Ball Variables
     [SerializeField] private List<GameObject> objectSpawnedList = new List<GameObject>();
     public int count = 5;
     public GameObject food;
-    /***/
+
     public Vector3 startCorner;
     public Vector3 endCorner;
     public int rows = 5;
     public int columns = 5;
-    /***/
-    // Agent Variables
+    public float rotationSpeed=4f;
+
     [SerializeField] private float moveSpeed = 6f;
     [SerializeField] private float jumpForce = 2f;
     private Rigidbody rb;
     private bool isJumping = false;
 
-
-    // Env Variables
-    [SerializeField] private Transform envLocation; // env
+    [SerializeField] private Transform envLocation;
     Material envMaterial;
-    public GameObject env; // plane
+    public GameObject env;
 
-
-
-    private bool IsCollectingBalls()
-    {
-        // Check if the agent is collecting balls
-        bool isCollectingBalls = objectSpawnedList.Count < count;
-        Debug.Log("isCollectingBalls: "+isCollectingBalls+", " + objectSpawnedList.Count+", count: "+count);
-
-        return isCollectingBalls;
-    }
-    public override void Initialize()
+    private void Awake()
     {
         rb = GetComponent<Rigidbody>();
-        // Ensure rb is not null before proceeding
         if (rb == null)
         {
             Debug.LogError("Rigidbody component is missing on the agent.");
-            return;
         }
-        envMaterial = env.GetComponent<Renderer>().material;
-        Debug.Log("Done Initialize");
+        else
+        {
+            envMaterial = env.GetComponent<Renderer>().material;
+            Debug.Log("Done Initialize");
+        }
     }
-    
+
     public override void OnEpisodeBegin()
     {
-        Debug.Log("New Episode");
+        ResetAgentPosition();
+        CreateBall();
+    }
 
-        // Spawn position Agent
+    private void ResetAgentPosition()
+    {
         transform.localPosition = GenerateRandomPosition();
-        
-        while (transform.localPosition == Vector3.zero) { transform.localPosition = GenerateRandomPosition(); }
-
-        // create a random number of coins spawn each round
+        while (transform.localPosition == Vector3.zero)
+        {
+            transform.localPosition = GenerateRandomPosition();
+        }
         count = Random.Range(2, 7);
         isJumping = false;
-
-        // Spawn position Ball       
-        CreateBall();
-
     }
 
     private void CreateBall()
     {
-        while(objectSpawnedList.Count != 0)
+        foreach (GameObject obj in objectSpawnedList)
         {
-            RemoveBall(objectSpawnedList);
+            Destroy(obj);
         }
+        objectSpawnedList.Clear();
         Debug.Log("create ball");
         SpawnBalls();
-
-        //for (int i = 0; i < count; i++)
-        //{
-        //    // Generate random position for the ball, avoiding obstacle positions
-        //    Vector3 ballPosition = GenerateRandomPosition();
-        //    while (ballPosition == Vector3.zero) { ballPosition = GenerateRandomPosition(); }
-
-        //    // Spawning 
-        //    GameObject newBall = Instantiate(food);
-
-        //    // Child of env
-        //    newBall.transform.parent = envLocation;
-
-        //    // Spawn in new Location
-        //    newBall.transform.localPosition = ballPosition;
-
-        //    // Add to list
-        //    objectSpawnedList.Add(newBall);
-        //}
         count = objectSpawnedList.Count;
     }
 
@@ -105,10 +74,8 @@ public class JumpTwo : Agent
         Vector3 randomPosition = Vector3.zero;
         bool positionFound = false;
 
-        // Generate random positions until a suitable one is found
         while (!positionFound)
         {
-            // Generate random position within environment bounds
             float xRand = Random.Range(-4f, 4f);
             float zRand = Random.Range(-4f, 4f);
             randomPosition = new Vector3(xRand, 0.3f, zRand);
@@ -121,42 +88,28 @@ public class JumpTwo : Agent
         return randomPosition;
     }
 
-    private void RemoveBall(List<GameObject> toBeDeletedGameObjectList)
-    {
-        foreach (GameObject i in toBeDeletedGameObjectList)
-        {
-            Destroy(i.gameObject);
-            count--;
-        }
-        toBeDeletedGameObjectList.Clear();
-        Debug.Log("Removed Ball");
-    }
-
     public override void CollectObservations(VectorSensor sensor)
     {
         sensor.AddObservation(transform.localPosition);
-        int newSize = 0;
         foreach (GameObject obj in objectSpawnedList)
         {
-            if(obj != null) { sensor.AddObservation(obj.transform.localPosition); } else { newSize++; }
-            
+            if (obj != null)
+            {
+                sensor.AddObservation(obj.transform.localPosition);
+            }
+            else
+            {
+                sensor.AddObservation(Vector3.zero);
+            }
         }
-        var emptySize = 15 - (objectSpawnedList.Count - newSize) * 3; // Since ball generation is dynamic to fill up the empty slots
-
-        while (emptySize > 0) { sensor.AddObservation(0f); --emptySize; }
     }
 
-
-    public override void OnActionReceived(ActionBuffers actions)
+      public override void OnActionReceived(ActionBuffers actions)
     {
-        
-        float moveX = actions.ContinuousActions[0];
-        float moveZ = actions.ContinuousActions[1];
-
+        float moveForward = actions.ContinuousActions[0];
+        float rotate = actions.ContinuousActions[1];
         bool jump = actions.DiscreteActions[0] == 1;
-       
 
-        // Jumping logic
         if (jump && CheckGrounded() && !isJumping)
         {
             isJumping = true;
@@ -164,65 +117,28 @@ public class JumpTwo : Agent
             Debug.Log("Jumping");
         }
         if (!jump) { isJumping = false; }
-        Vector3 velocity = new(moveX, 0.3f, moveZ);
-        velocity = moveSpeed * Time.deltaTime * velocity.normalized;
-        transform.localPosition += velocity;
-        // Add obstacle detection using ray perception
-        //if (IsDetectingObstacle())
-        //{
-        //    rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
 
+        // Rotate the agent
+        transform.Rotate(0, rotate * rotationSpeed * Time.deltaTime, 0);
 
-        //    // If obstacle detected within 0.5 units, trigger a jump
-        //    if (!isJumping)
-        //    {
-        //        isJumping = true;
-        //        //rb.AddForce(Vector3., ForceMode.Impulse);
-        //        Debug.Log("Jumping to avoid obstacle");
-        //    }
+        // Move the agent forward
+        Vector3 forwardMovement = transform.forward * moveForward * moveSpeed * Time.deltaTime;
+        rb.MovePosition(rb.position + forwardMovement);
 
-        //}
-        //// Penalize the agent for not collecting balls or moving towards the target
-        //if (!IsCollectingBalls())
-        //{
-        //    AddReward(-0.5f); // Penalize for idle behavior
-        //}
-       // if (JumpOverObstacle() && jump) { AddReward(2f); }
-        
-       
-
-        if (transform.position.y < -1) // !CheckGrounded() && transform.position.y < -1
+        if (transform.position.y < -1)
         {
-            Debug.Log("Not on ground");            
+            Debug.Log("Not on ground");
             envMaterial.color = Color.blue;
             AddReward(-3f);
-            // transform.localPosition = GenerateRandomPosition();
             EndEpisode();
         }
-
     }
-    private bool IsDetectingObstacle()    {
-       
-        var hitColliders = Physics.OverlapSphere(transform.position, 1f);
-        foreach (var hit in hitColliders) {
-            if (hit.CompareTag("Obstacle"))
-            {
-                Debug.Log("Obstacle detected");
-                return true;                
-            }
-        }
-        return false;
-
-    }
-
 
     private bool CheckGrounded()
     {
-        // Perform a raycast downward to check if the agent is grounded
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, Vector3.down, out hit, 2f)) // Adjust the distance according to your agent's height
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, 2f))
         {
-            // Check if the ray hits the ground layer or object
             if (hit.collider.CompareTag("Ground"))
             {
                 return true;
@@ -231,91 +147,53 @@ public class JumpTwo : Agent
         return false;
     }
 
-    private bool JumpOverObstacle()
-    {
-        // Perform a raycast downward to check if the agent is grounded
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, Vector3.down, out hit, 2f)) // Adjust the distance according to your agent's height
-        {
-            // Check if the ray hits the ground layer or object
-            if (hit.collider.CompareTag("Obstacle"))
-            {
-                Debug.Log("On Obstacle");
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public override void Heuristic(in ActionBuffers actionsOut) // Movement controlled by keyboard
-    {
-        ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
-        continuousActions[0] = Input.GetAxisRaw("Horizontal");
-        continuousActions[1] = Input.GetAxisRaw("Vertical");
-
-
-        ActionSegment<int> discreteActions = actionsOut.DiscreteActions;
-        discreteActions[0] = 0;
-        if (Input.GetKey(KeyCode.Space)) { discreteActions[0] = 1; Debug.Log("Jumping with space"); } else { discreteActions[0] = 0; }
-    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Ball"))
         {
-            // Remove from List
             objectSpawnedList.Remove(other.gameObject);
-            // Remove from World
             Destroy(other.gameObject);
-
             AddReward(3f);
             if (objectSpawnedList.Count == 0)
             {
                 envMaterial.color = Color.green;
-                RemoveBall(objectSpawnedList);
                 AddReward(2.5f);
                 EndEpisode();
             }
-
         }
-        if (other.gameObject.CompareTag("Wall"))
+        else if (other.gameObject.CompareTag("Wall"))
         {
             envMaterial.color = Color.red;
-            RemoveBall(objectSpawnedList);
             AddReward(-2f);
             EndEpisode();
             Debug.Log("Wall");
-
         }
-        //if (other.gameObject.CompareTag("Obstacle"))
-        //{
-        //    envMaterial.color = Color.red;
-        //    RemoveBall(objectSpawnedList);
-        //    AddReward(-1f);
-        //    EndEpisode();
-        //    Debug.Log("Obstacle");
-        //}
     }
+
     void SpawnBalls()
     {
-       
-    Vector3 widthVector = (endCorner - startCorner) / columns;
-        Vector3 heightVector = (Vector3.Cross(widthVector, Vector3.up).normalized * (Vector3.Distance(startCorner, endCorner) / rows)).normalized * Vector3.Distance(startCorner, endCorner) / rows;
+        Vector3 widthVector = (endCorner - startCorner) / columns;
+        Vector3 heightVector = Vector3.Cross(widthVector, Vector3.up).normalized * (Vector3.Distance(startCorner, endCorner) / rows);
 
         for (int i = 0; i < columns; i++)
         {
             for (int j = 0; j < rows; j++)
             {
-                float xRand = Random.Range(-5f, 12f);
-                float zRand = Random.Range(-6f, 6f);
-                Vector3 randomPosition = new Vector3(zRand, 0.3f, xRand);
-
-                Vector3 spawnPosition = startCorner + i * widthVector + j * heightVector + randomPosition;
+                Vector3 spawnPosition = startCorner + i * widthVector + j * heightVector;
                 spawnPosition.y = 0.3f;
-                // Add to list
                 objectSpawnedList.Add(Instantiate(food, spawnPosition, Quaternion.identity));
             }
-            
         }
         Debug.Log("Count =" + objectSpawnedList.Count);
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
+        continuousActions[0] = Input.GetAxisRaw("Vertical"); // Forward movement
+        continuousActions[1] = Input.GetAxisRaw("Horizontal"); // Rotation
+
+        ActionSegment<int> discreteActions = actionsOut.DiscreteActions;
+        discreteActions[0] = Input.GetKey(KeyCode.Space) ? 1 : 0;
     }
 }
